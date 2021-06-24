a221be1aad6067749fd62e6c501d3b2eb0adcbd2
Matthew Hansen <MY_NAME@example.comlearntocode711@gmail.comlscleargit config --global user.email MY_NAME@example.comlearntocode711@gmail.comclear>
2021-06-25 00:20:49 +1000

    server

--- src/App.svelte
+++ src/App.svelte
@@ -20,7 +20,7 @@
 </script>
 
 <main class="text-center">
-	<h1 class="bg-blue-900 pb-3 pt-8 text-5xl sm:text-7xl lg:text-9xl text-white ">CyberCoins</h1>
+	<h1 class="tracking-widest bg-blue-900 pb-3 pt-8 text-5xl sm:text-7xl lg:text-9xl text-white ">CyberCoins</h1>
 	<div class="flex justify-center bg-blue-900 py-2">
 		<svg on:click={refresh} xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-white bg-blue-900" viewBox="0 0 20 20" fill="currentColor">
   <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />

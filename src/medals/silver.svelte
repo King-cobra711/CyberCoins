f6050a9eba8625d10eaed29e52a61c9c4bf1f1f7
Matthew Hansen <MY_NAME@example.comlearntocode711@gmail.comlscleargit config --global user.email MY_NAME@example.comlearntocode711@gmail.comclear>
2021-06-26 17:59:58 +1000

    icon colour change

--- src/medals/silver.svelte
+++ src/medals/silver.svelte
@@ -1,48 +1,48 @@
-<svg class="w-3 h-3 absolute right-0 top-0" xml:space="preserve" viewBox="0 0 100 100" y="0" x="0" xmlns="http://www.w3.org/2000/svg" id="圖層_1" version="1.1" width="200px" height="200px" xmlns:xlink="http://www.w3.org/1999/xlink" style="width:25%;height:35%;background-size:initial;background-repeat-y:initial;background-repeat-x:initial;background-position-y:initial;background-position-x:initial;background-origin:initial;background-image:initial;background-color:rgb(229, 231, 235, var(--tw-text-opacity));background-clip:initial;background-attachment:initial;animation-play-state:paused" ><g class="ldl-scale" style="transform-origin:50% 50%;transform:rotate(0deg) scale(0.8, 0.8);animation-play-state:paused" ><g style="animation-play-state:paused" ><path fill="#8a8a8a" d="M62.5 30.5l-25 43.3c3.7 2.2 8.1 3.5 12.7 3.5 13.8 0 25-11.2 25-25 .1-9.4-5.1-17.6-12.7-21.8z" style="fill:rgb(138, 138, 138);animation-play-state:paused" ></path>
-<path fill="#999" d="M50.3 27.2c-13.8 0-25 11.2-25 25 0 9.1 4.9 17.1 12.3 21.5l25-43.3c-3.7-2-7.9-3.2-12.3-3.2z" style="fill:rgb(153, 153, 153);animation-play-state:paused" ></path>
-<path fill="#e0e0e0" d="M58 38.3L42 66c2.4 1.5 5.2 2.3 8.2 2.3 8.8 0 16-7.2 16-16 .1-6.1-3.2-11.3-8.2-14z" style="fill:rgb(224, 224, 224);animation-play-state:paused" ></path>
-<path fill="#fff" d="M50.3 36.2c-8.8 0-16 7.2-16 16 0 5.8 3.1 10.9 7.8 13.7l16-27.7c-2.4-1.2-5-2-7.8-2z" style="fill:rgb(255, 255, 255);animation-play-state:paused" ></path></g>
-<g style="animation-play-state:paused" ><path fill="#f5e169" d="M71.4 80.5s-2.7-1.8-2.7-4.1 2.7-4.1 2.7-4.1 2.7 1.8 2.7 4.1c0 2.2-2.7 4.1-2.7 4.1z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M73.9 77.4s1.8 2.7 4.1 2.7 4.1-2.7 4.1-2.7-1.8-2.7-4.1-2.7c-2.2 0-4.1 2.7-4.1 2.7z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M78 74s-3.1-1.1-3.7-3.3c-.6-2.2 1.5-4.7 1.5-4.7s3.1 1.1 3.7 3.3C80.2 71.5 78 74 78 74z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M79.7 70.4s2.5 2.1 4.7 1.5c2.2-.6 3.3-3.7 3.3-3.7s-2.5-2.1-4.7-1.5c-2.3.6-3.3 3.7-3.3 3.7z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M82.8 66.1s-3.2-.2-4.4-2.2.3-4.9.3-4.9 3.2.2 4.4 2.2c1.1 1.9-.3 4.9-.3 4.9z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M83.4 62.1s2.9 1.4 4.9.3c2-1.1 2.2-4.4 2.2-4.4s-2.9-1.4-4.9-.3c-2 1.2-2.2 4.4-2.2 4.4z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M85.3 57.1s-3.2.6-4.8-1c-1.6-1.6-1-4.8-1-4.8s3.2-.6 4.8 1 1 4.8 1 4.8z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M84.8 53.2s3.2.6 4.8-1c1.6-1.6 1-4.8 1-4.8s-3.2-.6-4.8 1c-1.6 1.6-1 4.8-1 4.8z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M85.4 47.9s-2.9 1.4-4.9.3c-2-1.1-2.2-4.4-2.2-4.4s2.9-1.4 4.9-.3c1.9 1.1 2.2 4.4 2.2 4.4z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M83.9 44.2s3.2-.2 4.4-2.2c1.1-2-.3-5-.3-5s-3.2.2-4.4 2.2c-1.1 2 .3 5 .3 5z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M83.1 38.9s-2.5 2.1-4.7 1.5c-2.2-.6-3.3-3.7-3.3-3.7s2.5-2.1 4.7-1.5c2.2.7 3.3 3.7 3.3 3.7z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M80.7 35.7s3.1-1.1 3.7-3.3-1.5-4.7-1.5-4.7-3.1 1.1-3.7 3.3c-.6 2.2 1.5 4.7 1.5 4.7z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M78.5 30.9s-1.8 2.7-4.1 2.7-4.1-2.7-4.1-2.7 1.8-2.7 4.1-2.7 4.1 2.7 4.1 2.7z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M75.4 28.4s2.7-1.8 2.7-4.1-2.7-4.1-2.7-4.1-2.7 1.8-2.7 4.1 2.7 4.1 2.7 4.1z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M72.1 24.2s-1.1 3.1-3.3 3.7-4.7-1.5-4.7-1.5 1.1-3.1 3.3-3.7c2.2-.6 4.7 1.5 4.7 1.5z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M68.4 22.6s2.1-2.5 1.5-4.7-3.7-3.3-3.7-3.3-2.1 2.5-1.5 4.7 3.7 3.3 3.7 3.3z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path stroke-miterlimit="10" stroke="#f5e169" fill="none" d="M68.6 22.3c10 6.2 16.6 17.2 16.6 29.8 0 15.4-10 28.5-23.8 33.2" style="stroke:rgb(245, 225, 105);animation-play-state:paused" ></path></g>
-<g style="animation-play-state:paused" ><path fill="#f5e169" d="M28.6 80.5s2.7-1.8 2.7-4.1-2.7-4.1-2.7-4.1-2.7 1.8-2.7 4.1c0 2.2 2.7 4.1 2.7 4.1z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M26.1 77.4s-1.8 2.7-4.1 2.7-4.1-2.7-4.1-2.7 1.8-2.7 4.1-2.7 4.1 2.7 4.1 2.7z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M22 74s3.1-1.1 3.7-3.3c.6-2.2-1.5-4.7-1.5-4.7s-3.1 1.1-3.7 3.3C19.8 71.5 22 74 22 74z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M20.3 70.4s-2.5 2.1-4.7 1.5-3.3-3.7-3.3-3.7 2.5-2.1 4.7-1.5 3.3 3.7 3.3 3.7z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M17.2 66.1s3.2-.2 4.4-2.2c1.1-2-.3-4.9-.3-4.9s-3.2.2-4.4 2.2c-1.1 1.9.3 4.9.3 4.9z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M16.6 62.1s-2.9 1.4-4.9.3c-2-1.1-2.2-4.4-2.2-4.4s2.9-1.4 4.9-.3c2 1.2 2.2 4.4 2.2 4.4z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M14.7 57.1s3.2.6 4.8-1c1.6-1.6 1-4.8 1-4.8s-3.2-.6-4.8 1-1 4.8-1 4.8z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M15.2 53.2s-3.2.6-4.8-1-1-4.8-1-4.8 3.2-.6 4.8 1 1 4.8 1 4.8z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M14.6 47.9s2.9 1.4 4.9.3c2-1.1 2.2-4.4 2.2-4.4s-2.9-1.4-4.9-.3c-1.9 1.1-2.2 4.4-2.2 4.4z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M16.1 44.2s-3.2-.2-4.4-2.2c-1.1-2 .3-5 .3-5s3.2.2 4.4 2.2c1.1 2-.3 5-.3 5z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M16.9 38.9s2.5 2.1 4.7 1.5c2.2-.6 3.3-3.7 3.3-3.7s-2.5-2.1-4.7-1.5c-2.2.7-3.3 3.7-3.3 3.7z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M19.3 35.7s-3.1-1.1-3.7-3.3 1.5-4.7 1.5-4.7 3.1 1.1 3.7 3.3c.6 2.2-1.5 4.7-1.5 4.7z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M21.5 30.9s1.8 2.7 4.1 2.7 4.1-2.7 4.1-2.7-1.8-2.7-4.1-2.7-4.1 2.7-4.1 2.7z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M24.6 28.4s-2.7-1.8-2.7-4.1 2.7-4.1 2.7-4.1 2.7 1.8 2.7 4.1c0 2.2-2.7 4.1-2.7 4.1z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M27.9 24.2s1.1 3.1 3.3 3.7 4.7-1.5 4.7-1.5-1.1-3.1-3.3-3.7-4.7 1.5-4.7 1.5z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path fill="#f5e169" d="M31.6 22.6s-2.1-2.5-1.5-4.7 3.7-3.3 3.7-3.3 2.1 2.5 1.5 4.7-3.7 3.3-3.7 3.3z" style="fill:rgb(245, 225, 105);animation-play-state:paused" ></path>
-<path stroke-miterlimit="10" stroke="#f5e169" fill="none" d="M31.4 22.3c-10 6.2-16.6 17.2-16.6 29.8 0 15.4 10 28.5 23.8 33.2" style="stroke:rgb(245, 225, 105);animation-play-state:paused" ></path></g>
-<metadata xmlns:d="https://loading.io/stock/" style="animation-play-state:paused" ><d:name style="animation-play-state:paused" >medal</d:name>
+<svg class="animate-wiggle w-3 h-3 absolute right-0 top-0" xml:space="preserve" viewBox="0 0 100 100" y="0" x="0" xmlns="http://www.w3.org/2000/svg" id="圖層_1" version="1.1" width="10px" height="10px" xmlns:xlink="http://www.w3.org/1999/xlink" style="width:25%;height:35%;background-size:initial;background-repeat-y:initial;background-repeat-x:initial;background-position-y:initial;background-position-x:initial;background-origin:initial;background-color:initial;background-clip:initial;background-attachment:initial;animation-play-state:running" ><g class="ldl-scale" style="transform-origin:50% 50%;transform:rotate(0deg) scale(0.8, 0.8);animation-play-state:running" ><g style="animation-play-state:running" ><path fill="#8a8a8a" d="M62.5 30.5l-25 43.3c3.7 2.2 8.1 3.5 12.7 3.5 13.8 0 25-11.2 25-25 .1-9.4-5.1-17.6-12.7-21.8z" style="fill:rgb(138, 138, 138);animation-play-state:running" ></path>
+<path fill="#999" d="M50.3 27.2c-13.8 0-25 11.2-25 25 0 9.1 4.9 17.1 12.3 21.5l25-43.3c-3.7-2-7.9-3.2-12.3-3.2z" style="fill:rgb(153, 153, 153);animation-play-state:running" ></path>
+<path fill="#e0e0e0" d="M58 38.3L42 66c2.4 1.5 5.2 2.3 8.2 2.3 8.8 0 16-7.2 16-16 .1-6.1-3.2-11.3-8.2-14z" style="fill:rgb(224, 224, 224);animation-play-state:running" ></path>
+<path fill="#fff" d="M50.3 36.2c-8.8 0-16 7.2-16 16 0 5.8 3.1 10.9 7.8 13.7l16-27.7c-2.4-1.2-5-2-7.8-2z" style="fill:rgb(255, 255, 255);animation-play-state:running" ></path></g>
+<g style="animation-play-state:running" ><path fill="#f5e169" d="M71.4 80.5s-2.7-1.8-2.7-4.1 2.7-4.1 2.7-4.1 2.7 1.8 2.7 4.1c0 2.2-2.7 4.1-2.7 4.1z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M73.9 77.4s1.8 2.7 4.1 2.7 4.1-2.7 4.1-2.7-1.8-2.7-4.1-2.7c-2.2 0-4.1 2.7-4.1 2.7z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M78 74s-3.1-1.1-3.7-3.3c-.6-2.2 1.5-4.7 1.5-4.7s3.1 1.1 3.7 3.3C80.2 71.5 78 74 78 74z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M79.7 70.4s2.5 2.1 4.7 1.5c2.2-.6 3.3-3.7 3.3-3.7s-2.5-2.1-4.7-1.5c-2.3.6-3.3 3.7-3.3 3.7z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M82.8 66.1s-3.2-.2-4.4-2.2.3-4.9.3-4.9 3.2.2 4.4 2.2c1.1 1.9-.3 4.9-.3 4.9z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M83.4 62.1s2.9 1.4 4.9.3c2-1.1 2.2-4.4 2.2-4.4s-2.9-1.4-4.9-.3c-2 1.2-2.2 4.4-2.2 4.4z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M85.3 57.1s-3.2.6-4.8-1c-1.6-1.6-1-4.8-1-4.8s3.2-.6 4.8 1 1 4.8 1 4.8z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M84.8 53.2s3.2.6 4.8-1c1.6-1.6 1-4.8 1-4.8s-3.2-.6-4.8 1c-1.6 1.6-1 4.8-1 4.8z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M85.4 47.9s-2.9 1.4-4.9.3c-2-1.1-2.2-4.4-2.2-4.4s2.9-1.4 4.9-.3c1.9 1.1 2.2 4.4 2.2 4.4z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M83.9 44.2s3.2-.2 4.4-2.2c1.1-2-.3-5-.3-5s-3.2.2-4.4 2.2c-1.1 2 .3 5 .3 5z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M83.1 38.9s-2.5 2.1-4.7 1.5c-2.2-.6-3.3-3.7-3.3-3.7s2.5-2.1 4.7-1.5c2.2.7 3.3 3.7 3.3 3.7z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M80.7 35.7s3.1-1.1 3.7-3.3-1.5-4.7-1.5-4.7-3.1 1.1-3.7 3.3c-.6 2.2 1.5 4.7 1.5 4.7z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M78.5 30.9s-1.8 2.7-4.1 2.7-4.1-2.7-4.1-2.7 1.8-2.7 4.1-2.7 4.1 2.7 4.1 2.7z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M75.4 28.4s2.7-1.8 2.7-4.1-2.7-4.1-2.7-4.1-2.7 1.8-2.7 4.1 2.7 4.1 2.7 4.1z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M72.1 24.2s-1.1 3.1-3.3 3.7-4.7-1.5-4.7-1.5 1.1-3.1 3.3-3.7c2.2-.6 4.7 1.5 4.7 1.5z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M68.4 22.6s2.1-2.5 1.5-4.7-3.7-3.3-3.7-3.3-2.1 2.5-1.5 4.7 3.7 3.3 3.7 3.3z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path stroke-miterlimit="10" stroke="#f5e169" fill="none" d="M68.6 22.3c10 6.2 16.6 17.2 16.6 29.8 0 15.4-10 28.5-23.8 33.2" style="stroke:rgb(40, 140, 0);animation-play-state:running" ></path></g>
+<g style="animation-play-state:running" ><path fill="#f5e169" d="M28.6 80.5s2.7-1.8 2.7-4.1-2.7-4.1-2.7-4.1-2.7 1.8-2.7 4.1c0 2.2 2.7 4.1 2.7 4.1z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M26.1 77.4s-1.8 2.7-4.1 2.7-4.1-2.7-4.1-2.7 1.8-2.7 4.1-2.7 4.1 2.7 4.1 2.7z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M22 74s3.1-1.1 3.7-3.3c.6-2.2-1.5-4.7-1.5-4.7s-3.1 1.1-3.7 3.3C19.8 71.5 22 74 22 74z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M20.3 70.4s-2.5 2.1-4.7 1.5-3.3-3.7-3.3-3.7 2.5-2.1 4.7-1.5 3.3 3.7 3.3 3.7z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M17.2 66.1s3.2-.2 4.4-2.2c1.1-2-.3-4.9-.3-4.9s-3.2.2-4.4 2.2c-1.1 1.9.3 4.9.3 4.9z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M16.6 62.1s-2.9 1.4-4.9.3c-2-1.1-2.2-4.4-2.2-4.4s2.9-1.4 4.9-.3c2 1.2 2.2 4.4 2.2 4.4z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M14.7 57.1s3.2.6 4.8-1c1.6-1.6 1-4.8 1-4.8s-3.2-.6-4.8 1-1 4.8-1 4.8z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M15.2 53.2s-3.2.6-4.8-1-1-4.8-1-4.8 3.2-.6 4.8 1 1 4.8 1 4.8z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M14.6 47.9s2.9 1.4 4.9.3c2-1.1 2.2-4.4 2.2-4.4s-2.9-1.4-4.9-.3c-1.9 1.1-2.2 4.4-2.2 4.4z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M16.1 44.2s-3.2-.2-4.4-2.2c-1.1-2 .3-5 .3-5s3.2.2 4.4 2.2c1.1 2-.3 5-.3 5z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M16.9 38.9s2.5 2.1 4.7 1.5c2.2-.6 3.3-3.7 3.3-3.7s-2.5-2.1-4.7-1.5c-2.2.7-3.3 3.7-3.3 3.7z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M19.3 35.7s-3.1-1.1-3.7-3.3 1.5-4.7 1.5-4.7 3.1 1.1 3.7 3.3c.6 2.2-1.5 4.7-1.5 4.7z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M21.5 30.9s1.8 2.7 4.1 2.7 4.1-2.7 4.1-2.7-1.8-2.7-4.1-2.7-4.1 2.7-4.1 2.7z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M24.6 28.4s-2.7-1.8-2.7-4.1 2.7-4.1 2.7-4.1 2.7 1.8 2.7 4.1c0 2.2-2.7 4.1-2.7 4.1z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M27.9 24.2s1.1 3.1 3.3 3.7 4.7-1.5 4.7-1.5-1.1-3.1-3.3-3.7-4.7 1.5-4.7 1.5z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path fill="#f5e169" d="M31.6 22.6s-2.1-2.5-1.5-4.7 3.7-3.3 3.7-3.3 2.1 2.5 1.5 4.7-3.7 3.3-3.7 3.3z" style="fill:rgb(40, 140, 0);animation-play-state:running" ></path>
+<path stroke-miterlimit="10" stroke="#f5e169" fill="none" d="M31.4 22.3c-10 6.2-16.6 17.2-16.6 29.8 0 15.4 10 28.5 23.8 33.2" style="stroke:rgb(40, 140, 0);animation-play-state:running" ></path></g>
+<metadata xmlns:d="https://loading.io/stock/" style="animation-play-state:running" ><d:name style="animation-play-state:running" >medal</d:name>
 
 
-<d:tags style="animation-play-state:paused" >badge,laurel,prize,winner,trophy,honor,silver,medal,web application</d:tags>
+<d:tags style="animation-play-state:running" >badge,laurel,prize,winner,trophy,honor,silver,medal,web application</d:tags>
 
 
-<d:license style="animation-play-state:paused" >by</d:license>
+<d:license style="animation-play-state:running" >by</d:license>
 
 
-<d:slug style="animation-play-state:paused" >dscy8u</d:slug></metadata></g><!-- generated by https://loading.io/ --></svg>
+<d:slug style="animation-play-state:running" >dscy8u</d:slug></metadata></g><!-- generated by https://loading.io/ --></svg>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,13 @@ module.exports = {
     extend: {
       animation: {
         "reverse-spin": "reverse-spin .3s linear infinite",
+        wiggle: "wiggle 1s ease-in-out infinite",
       },
       keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
         "reverse-spin": {
           from: {
             transform: "rotate(360deg)",
